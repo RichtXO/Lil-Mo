ca302288013d005d82526b26fad9f5e2e9a47971
Richard Tsai <richtxo@gmail.com>
2024-03-22 17:56:01 -0400

    Fixed error when reading json files during startup

--- src/main/java/com/richtxo/CommandRegistrar.java
+++ src/main/java/com/richtxo/CommandRegistrar.java
@@ -4,24 +4,19 @@ import discord4j.common.JacksonResources;
 import discord4j.discordjson.json.ApplicationCommandRequest;
 import discord4j.rest.RestClient;
 import discord4j.rest.service.ApplicationService;
-import org.checkerframework.checker.units.qual.A;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 import java.io.*;
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class CommandRegistrar {
 
     private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
     private final RestClient restClient;
-    private static final String COMMAND_FOLDER = "commands/";
+    private static final String COMMAND_REGEX = "commands/*.json";
 
     public CommandRegistrar(RestClient restClient) {
         this.restClient = restClient;
@@ -29,15 +24,17 @@ public class CommandRegistrar {
 
     protected void registerCmds() throws IOException {
         final JacksonResources mapper = JacksonResources.create();
+        PathMatchingResourcePatternResolver matcher = new PathMatchingResourcePatternResolver();
 
         final ApplicationService appService = restClient.getApplicationService();
         final long appID = restClient.getApplicationId().block();
-        final long guildID = Long.parseLong((System.getenv("TEST_SERVER") == null ? "9999" : System.getenv("TEST_SERVER")));
+        final long guildID = Long.parseLong(
+                System.getenv("TEST_SERVER") == null ? "9999" : System.getenv("TEST_SERVER"));
         List<ApplicationCommandRequest> commands = new ArrayList<>();
 
-        for (String json : listFiles()){
+        for (Resource resource : matcher.getResources(COMMAND_REGEX)){
             ApplicationCommandRequest request = mapper.getObjectMapper()
-                    .readValue(json, ApplicationCommandRequest.class);
+                    .readValue(resource.getInputStream(), ApplicationCommandRequest.class);
             commands.add(request);
         }
 
@@ -51,35 +48,5 @@ public class CommandRegistrar {
                     .doOnNext(cmd -> LOGGER.debug("Successfully registered Guild command: " + cmd.name()))
                     .doOnError(err -> LOGGER.error("Failed to register guild commands: " + err))
                     .subscribe();
-    }
-
-    private List<String> listFiles() throws IOException {
-        // Confirming command folder exists
-        URL url = CommandRegistrar.class.getClassLoader().getResource(COMMAND_FOLDER);
-        Objects.requireNonNull(url, COMMAND_FOLDER + " could not be found!");
-
-        // For Windows environment
-        Path path = Path.of(url.getPath().replace("/C:","C:"));
-        File[] jsons = new File(path.toString()).listFiles();
-        List<String> list = new ArrayList<>();
-
-        assert jsons != null;
-        for (File json : jsons){
-            String jsonToString = fileToString(COMMAND_FOLDER + json.getName());
-            list.add(Objects.requireNonNull(jsonToString, "Command file not found: " + json.getName()));
-        }
-
-        return list;
-    }
-
-    private static String fileToString(String fileName) throws IOException {
-        ClassLoader loader = ClassLoader.getSystemClassLoader();
-        try(InputStream inputStream = loader.getResourceAsStream(fileName)) {
-            if (inputStream == null) return null;
-            try (InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
-                 BufferedReader reader = new BufferedReader(inputStreamReader)) {
-                return reader.lines().collect(Collectors.joining(System.lineSeparator()));
-            }
-        }
     }
 }
