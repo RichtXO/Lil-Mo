021600374d8e83c8a33baf833cd22b5b0eb7b2cd
Richard Tsai <richtxo@gmail.com>
2024-03-27 19:33:56 -0400

    Removing unnecessary block() and cleanup

--- src/main/java/com/richtxo/commands/music/Queue.java
+++ src/main/java/com/richtxo/commands/music/Queue.java
@@ -38,76 +38,79 @@ public class Queue implements Command {
 
     @Override
     public Mono<Void> handle(ChatInputInteractionEvent event) {
-        Snowflake guildId = event.getInteraction().getGuildId().orElse(null);
-        List<AudioTrack> musicQueue = GuildAudioManager.of(guildId).getScheduler().getQueue();
-        User bot = Objects.requireNonNull(event.getClient().getSelf().block());
-        AudioTrackInfo current = GuildAudioManager.of(guildId).getPlayer().getPlayingTrack().getInfo();
+        return event.getClient().getSelf()
+                .flatMap(user -> {
+                    Snowflake guildId = event.getInteraction().getGuildId().orElse(Snowflake.of(0));
+                    List<AudioTrack> musicQueue = GuildAudioManager.of(guildId).getScheduler().getQueue();
 
-        if (musicQueue.isEmpty()){
-            if (GuildAudioManager.of(guildId).getPlayer().getPlayingTrack() == null)
-                return event.reply("No songs in queue!");
+                    if (musicQueue.isEmpty()){
+                        if (GuildAudioManager.of(guildId).getPlayer().getPlayingTrack() == null)
+                            return event.reply("No songs in queue!");
 
-            long totalHour = (current.length / (1000 * 60 * 60)) % 24;
-            long totalMin = (current.length / (1000 * 60)) % 60;
-            long totalSec = (current.length / 1000) % 60;
+                        AudioTrackInfo current = GuildAudioManager.of(guildId).getPlayer().getPlayingTrack().getInfo();
+                        long totalHour = (current.length / (1000 * 60 * 60)) % 24;
+                        long totalMin = (current.length / (1000 * 60)) % 60;
+                        long totalSec = (current.length / 1000) % 60;
 
-            EmbedCreateSpec.Builder currentSong = EmbedCreateSpec.builder()
-                    .title(String.format("Current song: %s", current.title))
-                    .color(Color.of(0xad0000))
-                    .thumbnail((current.artworkUrl==null ? bot.getAvatarUrl() : current.artworkUrl))
-                    .addField("Author", (current.author != null ? current.author : "N/A"), true)
-                    .addField("ISRC", (current.isrc != null ? current.isrc : "N/A"), true)
-                    .addField("URI", (current.uri != null ? current.uri : "N/A"), false)
-                    .footer(String.format("Total Time: %02d:%02d:%02d", totalHour, totalMin, totalSec),
-                            bot.getAvatarUrl());
+                        EmbedCreateSpec.Builder currentSong = EmbedCreateSpec.builder()
+                                .title(String.format("Current song: %s", current.title))
+                                .color(Color.of(0xad0000))
+                                .thumbnail((current.artworkUrl==null ? user.getAvatarUrl() : current.artworkUrl))
+                                .addField("Author", (current.author != null ? current.author : "N/A"), true)
+                                .addField("ISRC", (current.isrc != null ? current.isrc : "N/A"), true)
+                                .addField("URI", (current.uri != null ? current.uri : "N/A"), false)
+                                .footer(String.format("Total Time: %02d:%02d:%02d", totalHour, totalMin, totalSec),
+                                        user.getAvatarUrl());
 
-            return event.reply().withEmbeds(currentSong.build());
-        }
+                        return event.reply().withEmbeds(currentSong.build());
+                    }
 
-        long totalTime = current.length;
-        List<EmbedCreateSpec.Builder> paginationsBuilder = new ArrayList<>();
-        for (int pagination = 0; pagination < musicQueue.size() / 10; pagination++){
-            EmbedCreateSpec.Builder builder = getQueueBase(event, musicQueue, current);
-            for (int i = 0; i < Math.min(musicQueue.size() - (pagination * 10), 10); i++){
-                AudioTrackInfo track = musicQueue.get(i + (pagination * 10)).getInfo();
-                totalTime += track.length;
-                long min = (track.length / (1000 * 60)) % 60;
-                long sec = (track.length / 1000) % 60;
-                builder.addField(String.format("`%d.` `[%02d:%02d]` %s", i + 1, min, sec, track.title),
-                        "", false);
-            }
-            paginationsBuilder.add(builder);
-        }
+                    AudioTrackInfo current = GuildAudioManager.of(guildId).getPlayer().getPlayingTrack().getInfo();
+                    long totalTime = current.length;
+                    List<EmbedCreateSpec.Builder> paginationBuilder = new ArrayList<>();
+                    for (int pagination = 0; pagination < musicQueue.size() / 10; pagination++){
+                        EmbedCreateSpec.Builder builder = getQueueBase(event, musicQueue, current);
+                        for (int i = 0; i < Math.min(musicQueue.size() - (pagination * 10), 10); i++){
+                            AudioTrackInfo track = musicQueue.get(i + (pagination * 10)).getInfo();
+                            totalTime += track.length;
+                            long min = (track.length / (1000 * 60)) % 60;
+                            long sec = (track.length / 1000) % 60;
+                            builder.addField(String.format("`%d.` `[%02d:%02d]` %s", i + 1, min, sec, track.title),
+                                    "", false);
+                        }
+                        paginationBuilder.add(builder);
+                    }
 
-        List<EmbedCreateSpec> paginations = getBuilders(totalTime, paginationsBuilder, bot);
-        Button nextBtn = Button.secondary("next", ReactionEmoji.unicode("⏭️"));
-        Button prevBtn = Button.secondary("prev", ReactionEmoji.unicode("⏮️"));
-        Button doneDtn = Button.secondary("done", ReactionEmoji.unicode("✅"));
+                    List<EmbedCreateSpec> pagination = getBuilders(totalTime, paginationBuilder, user);
+                    Button nextBtn = Button.secondary("next", ReactionEmoji.unicode("⏭️"));
+                    Button prevBtn = Button.secondary("prev", ReactionEmoji.unicode("⏮️"));
+                    Button doneDtn = Button.secondary("done", ReactionEmoji.unicode("✅"));
 
-        AtomicInteger page = new AtomicInteger();
-        Mono<Void> buttonListener = event.getClient().on(ButtonInteractionEvent.class, buttonEvent -> {
-            String buttonId = buttonEvent.getCustomId();
-            if (buttonId.equals("next")){
-                page.addAndGet(1);
-                return buttonEvent.reply().withEmbeds(paginations.get(page.get())).withComponents(
-                        ActionRow.of(prevBtn, nextBtn.disabled(page.get() == paginations.size() - 1), doneDtn));
-            }
-            if (buttonId.equals("prev")){
-                page.decrementAndGet();
-                return buttonEvent.reply().withEmbeds(paginations.get(page.get())).withComponents(
-                        ActionRow.of(prevBtn.disabled(page.get() == 0), nextBtn, doneDtn));
-            }
+                    AtomicInteger page = new AtomicInteger();
+                    Mono<Void> buttonListener = event.getClient().on(ButtonInteractionEvent.class, buttonEvent -> {
+                        String buttonId = buttonEvent.getCustomId();
+                        if (buttonId.equals("next")){
+                            page.addAndGet(1);
+                            return buttonEvent.reply().withEmbeds(pagination.get(page.get())).withComponents(
+                                    ActionRow.of(prevBtn, nextBtn.disabled(page.get() == pagination.size() - 1), doneDtn));
+                        }
+                        if (buttonId.equals("prev")){
+                            page.decrementAndGet();
+                            return buttonEvent.reply().withEmbeds(pagination.get(page.get())).withComponents(
+                                    ActionRow.of(prevBtn.disabled(page.get() == 0), nextBtn, doneDtn));
+                        }
 
-            // When done button is pressed
-            return buttonEvent.reply(String.format("Exiting out of queue, %s!",
-                    Objects.requireNonNull(buttonEvent.getInteraction().getMember().orElse(null))
-                            .getNicknameMention()));
-        }).then();
+                        // When done button is pressed
+                        return buttonEvent.reply(String.format("Exiting out of queue, %s!",
+                                Objects.requireNonNull(buttonEvent.getInteraction().getMember().orElse(null))
+                                        .getNicknameMention()));
+                    }).then();
 
-        return event.reply()
-                .withEmbeds(paginations.getFirst())
-                .withComponents(ActionRow.of(prevBtn.disabled(), nextBtn, doneDtn))
-                .then(buttonListener);
+                    return event.reply()
+                            .withEmbeds(pagination.getFirst())
+                            .withComponents(ActionRow.of(prevBtn.disabled(), nextBtn, doneDtn))
+                            .then(buttonListener);
+                });
     }
 
     private EmbedCreateSpec.Builder getQueueBase(ChatInputInteractionEvent event, List<AudioTrack> musicQueue,
@@ -116,8 +119,8 @@ public class Queue implements Command {
                 .title(String.format("`%d` Total Songs in Queue", musicQueue.size() + 1))
                 .color(Color.of(0xad0000))
                 .description(String.format("Current song: `%s`", current.title))
-                .thumbnail(Objects.requireNonNull(
-                        event.getInteraction().getMember().orElse(null)).getAvatarUrl());
+                .thumbnail((event.getInteraction().getMember().isPresent() ?
+                        event.getInteraction().getMember().get().getAvatarUrl() : ""));
     }
 
     private static List<EmbedCreateSpec> getBuilders(
@@ -127,13 +130,13 @@ public class Queue implements Command {
         long totalMin = (totalTime / (1000 * 60)) % 60;
         long totalSec = (totalTime / 1000) % 60;
 
-        List<EmbedCreateSpec> paginations = new ArrayList<>();
+        List<EmbedCreateSpec> pagination = new ArrayList<>();
         for (int i = 0; i < paginationsBuilder.size(); i++){
             EmbedCreateSpec.Builder page = paginationsBuilder.get(i);
             page.footer(String.format("Total Time: [%02d:%02d:%02d] -- Page %d / %d",
                     totalHour, totalMin, totalSec, i + 1, paginationsBuilder.size()), bot.getAvatarUrl());
-            paginations.add(page.build());
+            pagination.add(page.build());
         }
-        return paginations;
+        return pagination;
     }
 }
