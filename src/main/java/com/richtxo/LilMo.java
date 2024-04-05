0cad5bed09ad84e4bcded0c9f97b0674385428d4
Richard Tsai <richtxo@gmail.com>
2024-04-05 02:16:44 -0400

    Adding log statements

--- src/main/java/com/richtxo/LilMo.java
+++ src/main/java/com/richtxo/LilMo.java
@@ -7,10 +7,12 @@ import com.sedmelluq.discord.lavaplayer.track.playback.NonAllocatingAudioFrameBu
 import discord4j.core.DiscordClientBuilder;
 import discord4j.core.GatewayDiscordClient;
 import discord4j.core.event.domain.interaction.ChatInputInteractionEvent;
+import discord4j.core.event.domain.lifecycle.ReadyEvent;
 import discord4j.core.object.presence.ClientActivity;
 import discord4j.core.object.presence.ClientPresence;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Mono;
 
 public class LilMo {
     public static final Logger LOGGER = LoggerFactory.getLogger(LilMo.class);
@@ -26,28 +28,32 @@ public class LilMo {
     }
 
     public static void main(String[] args){
-        final GatewayDiscordClient client = DiscordClientBuilder.create(System.getenv("TOKEN"))
-//                .onClientResponse(
-//                        ResponseFunction.retryWhen(
-//                                RouteMatcher.any(),
-//                                Retry.anyOf(SslHandler.class)))
-                .build()
+        // Checking if all env variables are present
+        if (System.getenv("TOKEN") == null) {
+            LOGGER.error("Discord Token not set!");
+            return;
+        }
+        if (System.getenv("SPOTIFY_CLIENT_ID") == null || System.getenv("SPOTIFY_SECRET") == null) {
+            LOGGER.error("Spotify credentials not set!");
+            return;
+        }
+
+        final GatewayDiscordClient client = DiscordClientBuilder.create(System.getenv("TOKEN")).build()
                 .gateway()
                 .setInitialPresence(s -> ClientPresence.online(
                         ClientActivity.listening("/play").withState("Give me more music!")))
-                .login()
-                .block();
+                .login().block();
 
         // Adding commands
         try {
             assert client != null;
             new CommandRegistrar(client.getRestClient()).registerCmds();
-        } catch (Exception e) {
-            LOGGER.error("Error trying to register commands: ", e);
-        }
+        } catch (Exception e) { LOGGER.error("Error trying to register commands: ", e); }
 
-        client.on(ChatInputInteractionEvent.class, Listener::handle)
-                .then(client.onDisconnect())
-                .block();
+        Mono.when(client.on(ReadyEvent.class).doOnNext(readyEvent -> LOGGER.info("Logged in as {}",
+                                        readyEvent.getSelf().getUsername())),
+                client.on(ChatInputInteractionEvent.class, Listener::handle),
+                client.onDisconnect().doOnTerminate(() -> LOGGER.info("Disconnected!")))
+            .block();
     }
 }
