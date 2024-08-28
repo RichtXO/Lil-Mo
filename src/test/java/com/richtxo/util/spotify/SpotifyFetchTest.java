aa00670e3ee420403733aad22d70b632c104d79a
Rich Tsai <richtxo@gmail.com>
2024-08-28 16:56:06 -0400

    Adding CI testings

---
+++ src/test/java/com/richtxo/util/spotify/SpotifyFetchTest.java
@@ -0,0 +1,46 @@
+package com.richtxo.util.spotify;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SpotifyFetchTest {
+
+    private final SpotifyFetch fetch = new SpotifyFetch();
+
+    @Test
+    void fetchSong() {
+        final SpotifySong valid =
+                fetch.fetchSong("https://open.spotify.com/track/4cOdK2wGLETKBW3PvgPWqT?si=8347518e3fd249f5");
+        assertNotNull(valid);
+        assertEquals("Never Gonna Give You Up", valid.getTitle());
+        assertEquals("Rick Astley", valid.getArtist()[0].getName());
+
+        assertNull(fetch.fetchSong("not_valid_spotify_link"));
+        assertNull(fetch.fetchSong(null));
+    }
+
+    @Test
+    void fetchPlaylist() {
+        final SpotifyPlaylist valid =
+                fetch.fetchPlaylist("https://open.spotify.com/playlist/51mPlHVdm7RAqdMACrOaXO?si=9ffd4363a75f4c41");
+        assertNotNull(valid);
+        assertEquals("Nostalgia", valid.getTitle());
+        assertNotEquals(0, valid.getSongs().length);
+
+        assertNull(fetch.fetchPlaylist(null));
+        assertNull(fetch.fetchPlaylist("not_valid_spotify_link"));
+    }
+
+    @Test
+    void fetchAlbum() {
+        final SpotifyPlaylist valid =
+                fetch.fetchAlbum("https://open.spotify.com/album/5Z9iiGl2FcIfa3BMiv6OIw?si=QdYDFg5nSDmoi5grrwHH7g");
+        assertNotNull(valid);
+        assertEquals("Whenever You Need Somebody", valid.getTitle());
+        assertEquals(10, valid.getSongs().length);
+
+        assertNull(fetch.fetchAlbum(null));
+        assertNull(fetch.fetchAlbum("not_valid_spotify_link"));
+    }
+}
